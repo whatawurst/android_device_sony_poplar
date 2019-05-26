e8c136a8ac88f98762d035c08d39069de8f00ef8
derfelot <frederic.koch@gmail.com>
2019-05-26 03:47:03 +0200

    poplar: Omnify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_poplar.mk
+    $(LOCAL_DIR)/omni_poplar.mk
--- device.mk
+++ device.mk
@@ -3,12 +3,9 @@ $(call inherit-product, device/sony/yoshino/platform.mk)
 ### PROPRIETARY VENDOR FILES
 $(call inherit-product, vendor/sony/poplar/poplar-vendor.mk)
 
-ifeq ($(WITH_FDROID),true)
-$(call inherit-product, vendor/fdroid/fdroid-vendor.mk)
-endif
-
 ### DALVIK
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, vendor/omni/config/phone-xxhdpi-2048-hwui-memory.mk)
 
 DEVICE_PATH := device/sony/poplar
 
@@ -19,9 +16,5 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
-ifeq ($(WITH_TWRP),true)
-include $(DEVICE_PATH)/device/init.mk
-else # WITH_TWRP
 include $(DEVICE_PATH)/device/*.mk
 include $(DEVICE_PATH)/vendor_prop.mk
-endif # WITH_TWRP
