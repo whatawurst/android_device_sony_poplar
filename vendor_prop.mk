16dd137e2d2a3088eb093f7aab4ea485c841a6b6
derfelot <5124143+derfelot@users.noreply.github.com>
2019-09-29 02:52:28 +0200

    poplar: Move common props to yoshino
    These were checked to be identitcal across the yoshino family - maple, poplar and lilac (including dsds variants).

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,37 +1,6 @@
-# Disable ZSL mode in camera
-PRODUCT_PROPERTY_OVERRIDES += \
-    camera.disable_zsl_mode=1
-
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480
-
-# Enable xfrm support
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.service.xrfm.supported=true \
-    persist.service.xrfm.mode=1
-
-# Path to frp partition
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.frp.pst=/dev/block/bootdevice/by-name/frp
-
-### PERFORMANCE
-# Library for power balancing
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so
-
-# Enable sched colocation
-PRODUCT_PROPERTY_OVERRIDES += \
-    sched.colocate.enable=1
-
-# Min/max cpu in core control
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.core_ctl_min_cpu=2 \
-    ro.vendor.qti.core_ctl_max_cpu=4
-
-# OEM Unlock reporting
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.oem_unlock_supported=1
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
